6659ca59198fcb75e630d74463f35a2867e608ce
MNdaba <44337261+MNdaba@users.noreply.github.com>
2018-11-03 08:32:19 +0200

    Update java_excerpt.java

--- java_excerpt.java
+++ java_excerpt.java
@@ -1,16 +1,12 @@
-ArrayList<Observation> allData = KMeans.readDataSet(filePath);//Read in dataset
+                ArrayList<Observation> allData = KMeans.readDataSet(filePath);//Read in dataset
 		int numOfFolds = 10;/**Used to split dataset into 10 samples of equal size (ensuring the proportion of -ve and +instances is preserved)*/
 		Observation[][] fold = new Observation[numOfFolds][(int) (allData.size() / numOfFolds)];
-		double[] accuracies = new double[numOfFolds];
-		Collections.shuffle(allData);
-		 
+		Collections.shuffle(allData);	 
 		ArrayList<Observation> positiveInstances = new ArrayList<Observation>();
 		ArrayList<Observation> negativeInstances = new ArrayList<Observation>();
-		int countPos = 0;
-		int countNeg = 0;
+		int countPos = 0; int countNeg = 0;
 		for(int k=0;k<allData.size();k++) {/**Split -ve and +ve instances*/
-			if(allData.get(k).getLabel()==1)positiveInstances.add(allData.get(k));
-			else negativeInstances.add(allData.get(k));
+			if(allData.get(k).getLabel()==1)positiveInstances.add(allData.get(k)); else negativeInstances.add(allData.get(k));
 		}
 			Collections.shuffle(positiveInstances);
 			Collections.shuffle(negativeInstances);
@@ -25,15 +21,12 @@ ArrayList<Observation> allData = KMeans.readDataSet(filePath);//Read in dataset
 				countNeg++;
 				}
 			}
-		}
-			
- 			 			
+		}	
  			ArrayList<Observation> trainSet = new ArrayList<Observation>();
 			ArrayList<Observation> testSet = new ArrayList<Observation>();
 			ArrayList<ArrayList<Observation>> data = new ArrayList<ArrayList<Observation>>();
-			for (int i = 0; i < numOfFolds; i++) {
-				data.add(new ArrayList<Observation>());
-			}
+			for (int i = 0; i < numOfFolds; i++) data.add(new ArrayList<Observation>());
+			
 		    /**data contains a list of 10 samples*/
 			for (int i = 0; i < numOfFolds; i++) {
 				for (int j = 0; j < (int) (allData.size() / numOfFolds); j++) {
